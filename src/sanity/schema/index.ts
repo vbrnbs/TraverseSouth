fc8a19d4096fb302bc09b2993ce744e786e3c1f0
vbrnbs <varszegibarnabas@gmail.com>
2026-06-17 22:10:59 +1200

    chore: remove deprecated adventure schema from Sanity

--- src/sanity/schema/index.ts
+++ src/sanity/schema/index.ts
@@ -1,7 +1,6 @@
 import destination from './destination'
 import activity from './activity'
 import operator from './operator'
-import adventure from './adventure'
 import itinerary from './itinerary'
 import { homepage } from './homepage'
 import { hero } from './hero'
@@ -11,7 +10,6 @@ export const schemaTypes = [
     destination,
     activity,
     operator,
-    adventure,
     itinerary,
     homepage,
     hero,
