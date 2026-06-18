c2a9fc80712bf4dd65601418a009c2732718b569
vbrnbs <varszegibarnabas@gmail.com>
2026-06-18 17:54:16 +1200

    feat: wrap NextStudio to add top margin preventing header overlap

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -4,5 +4,9 @@ import { NextStudio } from 'next-sanity/studio';
 import config from '../../../../sanity.config';
 
 export default function StudioPage() {
-  return <NextStudio config={config} />;
+  return (
+    <div style={{ marginTop: '72px', height: 'calc(100vh - 72px)', position: 'relative' }}>
+      <NextStudio config={config} />
+    </div>
+  );
 }
